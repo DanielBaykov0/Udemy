6862e25fb9484f6795028cb25d71341d9061c520
Daniel Baykov <99421433+DanielBaykov0@users.noreply.github.com>
2022-10-12 19:40:33 +0300

    Add files via upload

---
+++ BarkingDog/BarkingDog/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        BarkingDog.shouldWakeUp(true, 1);
+//        boolean wakeUP = true;
+//        wakeUP = BarkingDog.shouldWakeUp(true, 1);
+//        System.out.println(wakeUP);
+//
+//        wakeUP = BarkingDog.shouldWakeUp(false, 2);
+//        System.out.println(wakeUP);
+//
+//        wakeUP = BarkingDog.shouldWakeUp(true, 8);
+//        System.out.println(wakeUP);
+//
+//        wakeUP = BarkingDog.shouldWakeUp(true, -1);
+//        System.out.println(wakeUP);
+
+                boolean wakeUp = BarkingDog.shouldWakeUp(true, 1);
+                System.out.println(wakeUp);
+
+                boolean wakeUp2 = BarkingDog.shouldWakeUp(false, 2);
+                System.out.println(wakeUp2);
+
+                boolean wakeUp3 = BarkingDog.shouldWakeUp(true, 8);
+                System.out.println(wakeUp3);
+
+                boolean wakeUp4 = BarkingDog.shouldWakeUp(true, -1);
+                System.out.println(wakeUp4);
+
+
+
+            }
+        }
+
