d3d592e33da1867861bc46eb8efd67a6ea63b10e
Daniel Baykov <99421433+DanielBaykov0@users.noreply.github.com>
2022-10-29 13:11:35 +0300

    Adding Minutes to Years

---
+++ MinutesToYears/MinutesToYears/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        MinutesToYearsAndDays.printYearsAndDays(-525600);
+
+        MinutesToYearsAndDays.printYearsAndDays(1051200);
+
+        MinutesToYearsAndDays.printYearsAndDays(561600);
+        
+    }
+}
---
+++ MinutesToYears/MinutesToYears/src/MinutesToYearsAndDays.java
@@ -0,0 +1,14 @@
+public class MinutesToYearsAndDays {
+
+    public static void printYearsAndDays (long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long years = minutes / (365 * 24 * 60);
+            long days = (minutes / (24 * 60));
+            long remainingDays = days % 365;
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+        }
+    }
+}
+ //MinutesToYearsDaysCalculator
