308745eefec4b2b44b2a4b09193e2e3f5236c1b2
Daniel Baykov <99421433+DanielBaykov0@users.noreply.github.com>
2022-11-07 17:29:49 +0200

    Adding Playing Cat

---
+++ PlayingCat/PlayingCat/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(PlayingCat.isCatPlaying(true,45));
+
+        System.out.println(PlayingCat.isCatPlaying(false,36));
+
+        System.out.println(PlayingCat.isCatPlaying(false,35));
+    }
+}
