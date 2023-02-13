de744228f3de608677cf28c4c047528168ce4f00
Daniel Baykov <99421433+DanielBaykov0@users.noreply.github.com>
2023-02-13 14:46:16 +0200

    adding Positive, Negative, Zero

---
+++ PositiveNegativeZero/src/PositiveNegativeZero.java
@@ -0,0 +1,20 @@
+public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+
+        checkNumber(2);
+        checkNumber(-3);
+        checkNumber(0);
+
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else if(number == 0) {
+            System.out.println("zero");
+        }
+    }
+}
